05ded33b1e58c12c4736238ab08deabf86484391
Atsushi Iwasa <a-iwasa@m3.com>
2016-09-06 16:50:43 +0900

    add image for opened

--- Pod/Classes/LiquidFloatingActionButton.swift
+++ Pod/Classes/LiquidFloatingActionButton.swift
@@ -60,14 +60,32 @@ public class LiquidFloatingActionButton : UIView {
         }
     }
     
-    @IBInspectable public var image: UIImage? {
-        didSet {
-            if image != nil {
-                plusLayer.contents = image!.CGImage
-                plusLayer.path = nil
-            }
-        }
+    private var imageLayerRect: CGRect {
+        return CGRect(x: circleLayer.bounds.minX + circleLayer.bounds.width * 0.1,
+                      y: circleLayer.bounds.minY + circleLayer.bounds.height * 0.1,
+                      width: circleLayer.bounds.width * 0.8,
+                      height: circleLayer.bounds.width * 0.8)
     }
+    
+    private func updatePlusLayerFrame() {
+        plusLayer.frame = (image != nil || openedImage != nil) ? imageLayerRect : circleLayer.bounds
+    }
+    
+    @IBInspectable public var image: UIImage? { didSet {
+        if let image = image where isClosed {
+            plusLayer.contents = image.CGImage
+            plusLayer.path = nil
+        }
+        updatePlusLayerFrame()
+    }}
+    
+    @IBInspectable public var openedImage: UIImage? { didSet {
+        if let openedImage = openedImage where isOpening {
+            plusLayer.contents = openedImage.CGImage
+            plusLayer.path = nil
+        }
+        updatePlusLayerFrame()
+    }}
     
     @IBInspectable public var rotationDegrees: CGFloat = 45.0
 
@@ -98,26 +116,21 @@ public class LiquidFloatingActionButton : UIView {
     }
     
     private func cellArray() -> [LiquidFloatingCell] {
-        var result: [LiquidFloatingCell] = []
-        if let source = dataSource {
-            for i in 0..<source.numberOfCells(self) {
-                result.append(source.cellForIndex(i))
-            }
-        }
-        return result
+        guard let source = dataSource else { return [] }
+        return (0..<source.numberOfCells(self)).map { source.cellForIndex($0) }
     }
 
     // open all cells
     public func open() {
-        
-        // rotate plus icon
-        CATransaction.setAnimationDuration(0.8)
-        self.plusLayer.transform = CATransform3DMakeRotation((CGFloat(M_PI) * rotationDegrees) / 180, 0, 0, 1)
+        if let openedImage = openedImage {
+            plusLayer.contents = openedImage.CGImage
+        } else {
+            CATransaction.setAnimationDuration(0.8)
+            self.plusLayer.transform = CATransform3DMakeRotation((CGFloat(M_PI) * rotationDegrees) / 180, 0, 0, 1)
+        }
 
         let cells = cellArray()
-        for cell in cells {
-            insertCell(cell)
-        }
+        cells.forEach(insertCell)
 
         self.baseView.open(cells)
         
@@ -126,13 +139,14 @@ public class LiquidFloatingActionButton : UIView {
 
     // close all cells
     public func close() {
-        
-        // rotate plus icon
-        CATransaction.setAnimationDuration(0.8)
-        self.plusLayer.transform = CATransform3DMakeRotation(0, 0, 0, 1)
+        if let image = image {
+            plusLayer.contents = image.CGImage
+        } else {
+            CATransaction.setAnimationDuration(0.8)
+            self.plusLayer.transform = CATransform3DMakeRotation(0, 0, 0, 1)
+        }
     
         self.baseView.close(cellArray())
-        
         self.isClosed = true
     }
 
@@ -229,11 +243,7 @@ public class LiquidFloatingActionButton : UIView {
     }
 
     private func didTapped() {
-        if isClosed {
-            open()
-        } else {
-            close()
-        }
+        isClosed ? open() : close()
     }
     
     public func didTappedCell(target: LiquidFloatingCell) {
